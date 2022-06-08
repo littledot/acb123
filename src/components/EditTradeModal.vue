0ea262f50dbc045807b238f7aa19fe6374c0c386
littledot <littledot5566@gmail.com>
2022-06-08 14:39:13 -0400

    abstract button styles

--- src/components/EditTradeModal.vue
+++ src/components/EditTradeModal.vue
@@ -5,6 +5,7 @@ import * as s from './symbol'
 import * as u from './util'
 import TextInput from './core/TextInput.vue'
 import DateInput from './core/DateInput.vue'
+import Button from './core/Button.vue'
 import SelectInput from './core/SelectInput.vue'
 import NumberInput from './core/NumberInput.vue'
 import FxInput from './core/FxInput.vue'
@@ -107,9 +108,8 @@ function onClickBg(event: Event) {
             </h5>
             <button type="button"
                     class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-                    data-bs-dismiss="modal"
                     @click="onCancel"
-                    aria-label="Close"></button>
+                    aria-label="Close"/>
           </div>
           <div class="modal-body relative p-4">
             <form class="edit-trade-grid items-baseline gap-2
@@ -146,25 +146,21 @@ function onClickBg(event: Event) {
           </div>
           <div class="modal-footer flex flex-shrink-0 flex-wrap items-center p-4 border-t border-gray-200 rounded-b-md">
             <div class="flex-1">
-              <button type="button"
-                      class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
-                      @click="onDelete"
-                      data-bs-dismiss="modal">
+              <Button type="err"
+                      @click="onDelete">
                 Delete
-              </button>
+              </Button>
             </div>
             <div class="flex flex-row">
-              <button type="button"
-                      class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                      @click="onCancel"
-                      data-bs-dismiss="modal">
+              <Button type="pri"
+                      @click="onCancel">
                 Cancel
-              </button>
-              <button type="button"
-                      class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1"
+              </Button>
+              <Button class="ml-1"
+                      type="ok"
                       @click="onSave">
                 Save
-              </button>
+              </Button>
             </div>
           </div>
         </div>
---
+++ src/components/core/Button.vue
@@ -0,0 +1,28 @@
+<script setup lang='ts'>
+
+let props = defineProps<{
+  type: string,
+}>()
+
+const styles = {
+  pri: 'bg-blue-600 text-white shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg',
+  sec: 'bg-purple-600 text-white shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg active:bg-purple-800 active:shadow-lg',
+  ok: 'bg-green-500 text-white shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg active:bg-green-700 active:shadow-lg',
+  err: 'bg-red-600 text-white shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg active:bg-red-800 active:shadow-lg',
+  warn: 'bg-yellow-500 text-white shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg active:bg-yellow-700 active:shadow-lg',
+  info: 'bg-blue-400 text-white shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg active:bg-blue-600 active:shadow-lg',
+  light: 'bg-gray-200 text-gray-700 shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg active:bg-gray-400 active:shadow-lg',
+  dark: 'bg-gray-800 text-white shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg active:bg-gray-900 active:shadow-lg',
+  link: 'bg-transparent text-blue-600 hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-200',
+}
+
+</script>
+<template>
+  <button type="button"
+          class="inline-block px-6 py-2.5 font-medium text-xs leading-tight uppercase rounded focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+          :class="styles[type as keyof typeof styles]">
+    <slot />
+  </button>
+</template>
+<style scoped>
+</style>
