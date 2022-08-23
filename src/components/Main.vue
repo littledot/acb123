0c0327ade4245b7f5a0098e98b70b13de3c26049
littledot <littledot5566@gmail.com>
2022-08-23 13:23:52 -0400

    adjust MainView layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -39,123 +39,126 @@ function onShowEditModal(ticker: string, date: DateTime) {
 
 </script>
 <template>
-  <div class="flex flex-col">
-    <Teleport to="body">
-      <Modal title="Confirm Clear Data"
-             okLabel="Clear"
-             okStyle="err"
-             @ok="useTradeStore().clear()"
-             :show="showClearModal"
-             @hide="showClearModal = false">
-        <p>This will delete all trade data from the app.</p>
-        <p>This action cannot be reversed.</p>
-      </Modal>
-    </Teleport>
+  <Teleport to="body">
+    <Modal title="Confirm Clear Data"
+           okLabel="Clear"
+           okStyle="err"
+           @ok="useTradeStore().clear()"
+           :show="showClearModal"
+           @hide="showClearModal = false">
+      <p>This will delete all trade data from the app.</p>
+      <p>This action cannot be reversed.</p>
+    </Modal>
+  </Teleport>
 
-    <Teleport to="body">
-      <ImportTradesModal title="Import Trades"
-                         okLabel="Import"
-                         okStyle="ok"
-                         @ok=""
-                         :show="showImportModal"
-                         @hide="showImportModal = false" />
-    </Teleport>
+  <Teleport to="body">
+    <ImportTradesModal title="Import Trades"
+                       okLabel="Import"
+                       okStyle="ok"
+                       @ok=""
+                       :show="showImportModal"
+                       @hide="showImportModal = false" />
+  </Teleport>
 
-    <Teleport to="body">
-      <EditTradeModal :security="editModalTickerField"
-                      :date="editModalDateField"
-                      :show="showEditModal"
-                      @hide="showEditModal = false" />
-    </Teleport>
+  <Teleport to="body">
+    <EditTradeModal :security="editModalTickerField"
+                    :date="editModalDateField"
+                    :show="showEditModal"
+                    @hide="showEditModal = false" />
+  </Teleport>
 
-    <div class="flex flex-row">
-      <div class="flex-1 flex flex-row">
-        <Button type="pri"
-                @click="importExample">
-          Import Example
-        </Button>
-        <Button type="pri"
-                @click="showImportModal = true">
-          Import Trades (CSV)
+  <div v-bind="$attrs"
+       class="flex-col items-center mt-4">
+    <div class="w-[90%]">
+      <div class="w-full flex">
+        <div class="flex-1 flex">
+          <Button type="pri"
+                  @click="importExample">
+            Import Example
+          </Button>
+          <Button type="pri"
+                  @click="showImportModal = true">
+            Import Trades (CSV)
+          </Button>
+        </div>
+        <Button type="err"
+                class="self-end"
+                @click="showClearModal = true">
+          Clear Data
         </Button>
       </div>
-      <Button type="err"
-              class="self-end"
-              @click="showClearModal = true">
-        Clear Data
-      </Button>
-    </div>
 
-    <div id="tradeAccordion"
-         class="accordion">
-      <Accordion v-for="([__, hists], i) of ui.orphanTrades"
-                 id="orphan">
-        <template #header>
-          <div class="flex flex-col flex-1">
-            <div class="text-xl">Ungrouped Trades</div>
-            <div class="">Trades: {{ u.fmt(hists.tradeCount) }}</div>
-          </div>
-        </template>
+      <div id="tradeAccordion"
+           class="accordion w-full mt-4">
+        <Accordion v-for="([__, hists], i) of ui.orphanTrades"
+                   id="orphan">
+          <template #header>
+            <div class="flex-col flex-1">
+              <div class="text-xl">Ungrouped Trades</div>
+              <div class="">Trades: {{ u.fmt(hists.tradeCount) }}</div>
+            </div>
+          </template>
 
-        <template #body>
-          <Accordion v-for="([security, events], j) of hists.tickerTrades"
-                     :id="`orphan-${i}-${j}`"
-                     :key="security">
-            <template #header>
-              <div class="flex flex-col flex-1 pl-2.5">
-                <div class="text-xl">{{ security }}</div>
-                <div class="">Capital Gains: {{ events.yearGains.format() }} </div>
-                <div class="">Trades: {{ u.fmt(events.tradeCount) }}</div>
-              </div>
-            </template>
+          <template #body>
+            <Accordion v-for="([security, events], j) of hists.tickerTrades"
+                       :id="`orphan-${i}-${j}`"
+                       :key="security">
+              <template #header>
+                <div class="flex-col flex-1 pl-2.5">
+                  <div class="text-xl">{{ security }}</div>
+                  <div class="">Capital Gains: {{ events.yearGains.format() }} </div>
+                  <div class="">Trades: {{ u.fmt(events.tradeCount) }}</div>
+                </div>
+              </template>
 
-            <template #body>
-              <div class="accordion-body py-4 px-5">
-                <EventTimeline :events="events" />
-              </div>
-            </template>
-          </Accordion>
-        </template>
-      </Accordion>
+              <template #body>
+                <div class="accordion-body py-4 px-5">
+                  <EventTimeline :events="events" />
+                </div>
+              </template>
+            </Accordion>
+          </template>
+        </Accordion>
 
-      <Accordion v-for="([year, hists], i) of ui.tradeHistory"
-                 :id="`` + i"
-                 :key="year">
-        <template #header>
-          <div class="flex flex-col flex-1">
-            <div class="text-xl">{{ year }}</div>
-            <div class="">Capital Gains: {{ hists.yearGains.format() }} </div>
-            <div class="">Trades: {{ u.fmt(hists.tradeCount) }}</div>
-          </div>
-        </template>
+        <Accordion v-for="([year, hists], i) of ui.tradeHistory"
+                   :id="`` + i"
+                   :key="year">
+          <template #header>
+            <div class="flex-col flex-1">
+              <div class="text-xl">{{ year }}</div>
+              <div class="">Capital Gains: {{ hists.yearGains.format() }} </div>
+              <div class="">Trades: {{ u.fmt(hists.tradeCount) }}</div>
+            </div>
+          </template>
 
-        <template #body>
-          <Accordion v-for="([security, events], j) of hists.tickerTrades"
-                     :id="`${i}-${j}`"
-                     :key="security">
-            <template #header>
-              <div class="flex flex-col flex-1 pl-2.5">
-                <div class="text-xl">{{ security }}</div>
-                <div class="">Capital Gains: {{ events.yearGains.format() }} </div>
-                <div class="">Trades: {{ u.fmt(events.tradeCount) }}</div>
-              </div>
-              <!-- @click.stop does not work!? -->
-              <!-- https://stackoverflow.com/a/70664716 -->
-              <Icon class="w-6 h-6 ml-2 mr-1 fill-blue-600"
-                    :path="mdiPlus"
-                    @click="onShowEditModal(security, DateTime.local(year, 1, 1))"
-                    data-bs-toggle="collapse"
-                    data-bs-target />
-            </template>
+          <template #body>
+            <Accordion v-for="([security, events], j) of hists.tickerTrades"
+                       :id="`${i}-${j}`"
+                       :key="security">
+              <template #header>
+                <div class="flex-col flex-1 pl-2.5">
+                  <div class="text-xl">{{ security }}</div>
+                  <div class="">Capital Gains: {{ events.yearGains.format() }} </div>
+                  <div class="">Trades: {{ u.fmt(events.tradeCount) }}</div>
+                </div>
+                <!-- @click.stop does not work!? -->
+                <!-- https://stackoverflow.com/a/70664716 -->
+                <Icon class="w-6 h-6 ml-2 mr-1 fill-blue-600"
+                      :path="mdiPlus"
+                      @click="onShowEditModal(security, DateTime.local(year, 1, 1))"
+                      data-bs-toggle="collapse"
+                      data-bs-target />
+              </template>
 
-            <template #body>
-              <div class="accordion-body py-4 px-5">
-                <EventTimeline :events="events" />
-              </div>
-            </template>
-          </Accordion>
-        </template>
-      </Accordion>
+              <template #body>
+                <div class="accordion-body py-4 px-5">
+                  <EventTimeline :events="events" />
+                </div>
+              </template>
+            </Accordion>
+          </template>
+        </Accordion>
+      </div>
     </div>
   </div>
 </template>
